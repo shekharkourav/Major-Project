3f071f8737e662f42cc2ebaadffe48adcadc3a20
Shekhar Kourav <137866222+shekharkourav@users.noreply.github.com>
2024-01-06 01:01:54 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
         req.flash("error","you must be logged in to create post");
-        res.redirect("/login");
+        return res.redirect("/login");
     }
     next();
 };
